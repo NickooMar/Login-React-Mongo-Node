d0cded878c3d6de3be18f61d111b6ee09333deb8
Nico <nicoo.marsili@gmail.com>
2020-12-06 22:39:01 -0300

    gitIgnore

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import dotenv from 'dotenv'
 import ReactDOM from 'react-dom';
 import App from './App';
 import Context from './Pages/Context'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./main.css";
-
-dotenv.config()
 
 ReactDOM.render(
   <React.StrictMode>
