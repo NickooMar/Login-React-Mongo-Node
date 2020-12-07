2b661040e97e270152d91547642b920472b8a703
Nico <nicoo.marsili@gmail.com>
2020-12-06 22:32:01 -0300

    Ignore some files

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import dotenv from 'dotenv'
 import ReactDOM from 'react-dom';
 import App from './App';
 import Context from './Pages/Context'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./main.css";
+
+dotenv.config()
 
 ReactDOM.render(
   <React.StrictMode>
